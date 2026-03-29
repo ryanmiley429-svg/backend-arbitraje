6417ba1ef10d7cc091a9b916dc4863f8b7a0496d
Jonathan <ryanmiley429@gmail.com>
2026-03-29 01:20:13 -0300

    agrego endpoint arbitraje

--- server.js
+++ server.js
@@ -13,6 +13,27 @@ app.get("/test", (req, res) => {
     mensaje: "API funcionando correctamente"
   });
 });
+app.get("/arbitraje", (req, res) => {
+  const binance = 65000;
+  const bybit = 65200;
+
+  const diferencia = bybit - binance;
+
+  const comision = 0.001;
+  const feeCompra = binance * comision;
+  const feeVenta = bybit * comision;
+  const transferencia = 10;
+
+  const ganancia = diferencia - feeCompra - feeVenta - transferencia;
+
+  res.json({
+    binance,
+    bybit,
+    diferencia,
+    ganancia,
+    rentable: ganancia > 0
+  });
+});
 
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto " + PORT);
